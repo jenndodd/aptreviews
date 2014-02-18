8653e7262ded1c0b7207f0494df6f35d94b9eaf6
My Name <dodd.jl@gmail.com>
2014-02-17 20:47:20 -0500

    Oops big commit. Created nav bar, refactored add a listing, began styling apartment show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,6 +8,20 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.create(url: params[:listing][:url], apartment_id: params[:apartment_id])
 
-    redirect_to ("/apartments/#{ params[:apartment_id] }")
+    redirect_to apartment_path(params[:apartment_id])
+  end
+
+  def edit
+    @update_worked = true
+  end
+
+  def update
+    @update_worked = @review.update(review_params)
+    
+    if @update_worked
+      redirect_to apartment_path(params[:apartment_id])
+    else
+      render(:edit)
+    end
   end
 end
